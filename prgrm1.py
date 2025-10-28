4d221041442a85c9bfee25e3fdf2c10839118c99
yashaswini-05506 <yashaswimadnallimath@gmail.com>
2025-10-28 09:19:04 +0530

    save

---
+++ prgrm1.py
@@ -0,0 +1,11 @@
+# Addition of two numbers with predefined values
+
+# Assign values
+a = 10
+b = 20
+
+# Calculate the sum
+sum = a + b
+
+# Display the result
+print("The sum of", a, "and", b, "is", sum)
